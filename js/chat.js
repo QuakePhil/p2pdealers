0f0c76cccda4363be5839b0dc3b3b3f7dac17a9a
QuakePhil <phil.nyc@gmail.com>
2017-12-07 13:23:56 +0000

    Refactoring

---
+++ js/chat.js
@@ -0,0 +1,41 @@
+var backlog = 'Welcome to O.G. Chat 1.0\n';
+
+function sendchat(blah) {
+  blah = blah + '\n';
+  document.getElementById('chat').value += blah;
+  backlog += blah;
+  send('say|' + blah);
+}
+
+function blahblah() {
+  let blah = gang.name + ': ' + document.getElementById('message').value;
+  sendchat(blah);
+  document.getElementById('message').value = '';
+}
+
+function chat() {
+  let div = dce('div');
+  let input = dce('input');
+  let blah = dce('button');
+  let textarea = dce('textarea');
+
+  div.className = 'chat';
+  blah.appendChild(dct('Blah'));
+  blah.addEventListener('click', blahblah, false);
+
+  input.type = 'text';
+  input.id = 'message';
+  input.addEventListener('keypress', function(e) {
+    if (e.key == 'Enter') blahblah();
+  }, false);
+
+  textarea.id = 'chat';
+  textarea.value = backlog;
+  textarea.disabled = true;
+
+  div.appendChild(textarea);
+  div.appendChild(dce('br'));
+  div.appendChild(input);
+  div.appendChild(blah);
+  return div;
+}
---
+++ js/dom.js
@@ -0,0 +1,11 @@
+function dce(what) {
+  return document.createElement(what);
+}
+
+function dct(what) {
+  return document.createTextNode(what);
+}
+
+function dge(id) {
+  return document.getElementById(id);
+}
